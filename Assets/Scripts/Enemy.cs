f1e9ec30d2e198fd7c5610ed573d9bca0c4f447b
Aloux01757 <arthurspring2@gmail.com>
2023-01-31 13:05:46 -0300

    added comments and new script "SelfDestruct"

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+    public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] Transform parent;
     void OnParticleCollision(GameObject other) 
     {
-        Destroy(this.gameObject);
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity); // instancia o VFX e declara variavel
+        vfx.transform.parent = parent;  // pega variavel VFX e coloca numa parent (Children)
+        Destroy(this.gameObject); // Destroi o inimigo como o todo
     }
 }
